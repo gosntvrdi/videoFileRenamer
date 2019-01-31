e60f809c38efde72483253ab36b3bed9cfd2501b
father.luka@gmail.com <mihica.909>
2019-01-31 21:47:14 +0100

    dsd

--- videoFileRenamer.py
+++ videoFileRenamer.py
@@ -2,16 +2,20 @@ import os
 import string
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-morningDir = '/videos/morning'
-dayDir = '/videos/day'
 
-def videoFileRenamer(location):
-    for fileName in os.listdir(location):
-        newFileName = ''.join(c for c in fileName if c in string.printable)
-        os.rename(os.path.join(location,fileName), os.path.join(src_dir, newFileName))
+dirname = os.path.dirname(__file__)
+morningDir = (dirname + '/videos/morning')
+dayDir = (dirname + '/videos/day')
+bad_chars = [ '$', '#', '!', '"', '%', ')', '(', '[', ']', '@', '=', '☃', '', '', '', '♥', '»', '', '◐', 'ネ',
+            'マ', 'ノ', 'ッ', 'ト', '・', ' ', '☾', '☀']
+
+def videoFileRenamer(path):
+    for dir, subdir, files in os.walk(path):
+        for file in files:
+            os.rename(os.path.join(dir,file), os.path.join(dir, "".join(filter(lambda x:x not in bad_chars, file))))
 
 
-
-scheduler = BlockingScheduler()
-scheduler.add_job(dlVideo, trigger='interval', hours=1, max_instances=1)
-scheduler.start()
+videoFileRenamer(dayDir)
+# scheduler = BlockingScheduler()
+# scheduler.add_job(videoFileRenamer, trigger='interval', hours=1, max_instances=1)
+# scheduler.start()
