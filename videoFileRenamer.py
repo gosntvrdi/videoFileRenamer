32ce345b7aa9f473f353bde74ca14d46fd42f017
gosntvrdi <father.luka@gmail.com>
2019-01-31 20:30:15 +0100

    jh

--- videoFileRenamer.py
+++ videoFileRenamer.py
@@ -5,10 +5,12 @@ import string
 morningDir = '/media/songftptodb/morning'
 dayDir = '/media/songftptodb/day'
 
-def videoFileRenamer(directory):
-    for fileName in os.listdir(directory):
+def videoFileRenamer(location):
+    for fileName in os.listdir(location):
         newFileName = ''.join(c for c in fileName if c in string.printable)
-        os.rename(os.path.join(directory,fileName), os.path.join(src_dir, newFileName))
+        os.rename(os.path.join(location,fileName), os.path.join(src_dir, newFileName))
+
+
 
 videoFileRenamer(morningDir)
 videoFileRenamer(dayDir)
